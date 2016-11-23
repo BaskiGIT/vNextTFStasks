8242a8ee4bd834d85202aec4dceb0ede1c8c5c7a
Baskar Lingam <bskr.psgtech@gmail.com>
2016-11-23 22:42:04 +0100

    vNextTFSbuildTasks

---
+++ GetSystemDrivesInfo/GetSystemDrivesInfo.ps1
@@ -0,0 +1,19 @@
+﻿##############
+
+# Author        : Baskar Lingam Ramachandran (B.Ramachandran@iaea.org)
+# Created Date  : 18th Nov 2016
+
+##############
+
+$server = $env:computername
+
+$format = @{Expression={$_.DeviceID};Label="Drive"},`
+@{Expression={$_.Size / 1GB};Label="Total Size"},`
+@{Expression={$_.FreeSpace};Label="Free Space"}
+
+$Drive = get-wmiobject win32_logicaldisk | Select-Object Size, FreeSpace, DeviceID # | Format-Table $format
+
+foreach ($item in $Drive)
+{
+    Write-Host "`t-----$($item.DeviceID) has $([math]::Round($item.FreeSpace/1GB,2)) GB free space of the total space of $([math]::Round($item.Size / 1GB,2)) GB"
+}
